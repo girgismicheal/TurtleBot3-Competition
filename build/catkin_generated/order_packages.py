b51919f708ac09bc1f25496eecd13ca4f48cbf4c
Girgis Micheal <36516704+girgismicheal@users.noreply.github.com>
2023-05-20 21:34:26 +0300

    Last version

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/girgis/Desktop/foot_ball_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/girgis/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/girgis/catkin_ws/devel;/opt/ros/noetic' != '' else []
